bdd6d1511623ad5e53d264f1cafc9cc9b6963f40
ajay <ajay@scorich.com>
2024-11-04 18:34:53 +0530

    all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    root: "./",
-    build: {
-        outDir: "build",
-    },
-    base: "",
+    // root: "./",
+    // build: {
+    //     outDir: "build",
+    // },
+    // base: "",
     publicDir: "public",
     server: {
         port: 4000, 
